877fae381a67d582ece941f1bcac9fe6b1dffe44
hamza <taoussi.hamza.dev@gmail.com>
2023-08-20 17:56:45 +0100

    first commmit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    swcMinify: true,
+    compress: true,
+    experimental: {
+        serverActions: true
+    }
+}
 
 module.exports = nextConfig
